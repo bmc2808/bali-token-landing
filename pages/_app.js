e5643c692c58e1032e3aea90168591ee28862bb9
bmc2808 <admin@balimemecoin.com>
2025-03-24 16:39:16 +0700

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,1 @@
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
